f8b222709854d78d0a3c4e9601052c53f22e243d
F3X <zakirobloxzakirobloxdemon123@gmail.com>
2025-05-11 15:37:35 +0100

    Update #1 Your First Lua Code.lua

--- ENGLISH LESSONS/#1 Your First Lua Code.lua
+++ ENGLISH LESSONS/#1 Your First Lua Code.lua
@@ -1,1 +1,19 @@
-.
+--[[ 
+Hey there! Let me introduce you to Lua - a super cool programming language that's easy to learn and really powerful. I remember when I first started with Lua, I was amazed at how simple yet capable it is. Lua is especially popular in game development (like Roblox!) but has many other uses too.
+
+#1 Lesson: Your First Lua/Luau Code
+]]
+
+-- This makes the output say "Hello!"
+print("Hello!") 
+
+-- This stores your name
+local myName = "Ahmed" 
+print("My name is " .. myName)
+
+--[[
+---- What's Happening?!: ----
+1- print() shows text on the output
+2- local creates a variable (like a labeled box)
+3- .. combines text (like glue for words)
+]]
