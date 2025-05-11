d0b27a5f61952126818e354b79895a5f7b2f2519
F3X <zakirobloxzakirobloxdemon123@gmail.com>
2025-05-11 15:56:26 +0100

    Create #2 Variables.lua

---
+++ ENGLISH LESSONS/#2 Variables.lua
@@ -0,0 +1,12 @@
+local name = "Mahmoud" -- Stores text
+local age = 20 -- Stores numbers
+local isStudent = true -- Stores true/false
+print(name, age) -- Prints variable values
+
+--[[ 
+---- Explanation: ----
+1- local creates a variable
+2- = assigns a value
+3- Variable names should be descriptive
+4- Lua automatically knows the type (text/number/true-false)
+]]
