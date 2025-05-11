401ca44cca3ced735b7b15e23f28b782f481ac29
F3X <zakirobloxzakirobloxdemon123@gmail.com>
2025-05-11 15:53:48 +0100

    Update #1 Printing Messages.lua

--- ENGLISH LESSONS/#1 Printing Messages.lua
+++ ENGLISH LESSONS/#1 Printing Messages.lua
@@ -2,8 +2,14 @@
 What we'll learn?!: making the output displays text
 ]]
 
--- this makes the output show text
-print("Hello World!")
+print("Hello World!") -- Prints Text Between the quotes
+print("25") -- Prints numbers directly
+print("2 + 3 =", 2+3) -- can print mixed content
 
--- you can print numbers too
-print("123")
+--[[
+---- Explanation: ----
+1- print() displays whatever you put inside the parentheses
+2- Text must be in quotes ""
+3- Numbers can be printed directly
+4- Commas , separate multiple items
+]]
